d5d98b8cb0e585dc912f6c72e5726d3fd13182c8
Ssteynbreher <Ssteynbreher@mail.ru>
2025-09-25 16:29:34 +0700

    Добавила разделенные файлы и обновила файл README

---
+++ src/Movable.kt
@@ -0,0 +1,6 @@
+interface Movable {
+    var x: Double
+    var y: Double
+    var speed: Double
+    fun move()
+}
--- src/main.kt
+++ src/main.kt
@@ -1,56 +1,6 @@
 import kotlin.concurrent.thread
 import kotlin.random.Random
 
-interface Movable {
-    var x: Double
-    var y: Double
-    var speed: Double
-    fun move()
-}
-open class Human(
-    var fullname: String,
-    var age: Int,
-    override var speed: Double,
-    var group_number: Int,
-) : Movable {
-    override var x: Double = 0.0
-    override var y: Double = 0.0
-    init {
-        println("Создан человек: $fullname, возраст: $age, номер группы: $group_number")
-    }
-    override fun move() {
-        val directionX = Random.nextDouble(-1.0, 1.0)
-        val directionY = Random.nextDouble(-1.0, 1.0)
-        x += directionX * speed
-        y += directionY * speed
-        println("$fullname переместился в позицию: ${getPosition()}")
-    }
-    fun getPosition(): String = "(${"%.2f".format(x)}, ${"%.2f".format(y)})"
-}
-class Driver(
-    fullname: String,
-    age: Int,
-    override var speed: Double,
-    group_number: Int,
-    var direction: String = "right"
-) : Human(fullname, age, speed, group_number) {
-
-    override fun move() {
-        when (direction) {
-            "up" -> y += speed
-            "down" -> y -= speed
-            "left" -> x -= speed
-            "right" -> x += speed
-        }
-        println("Водитель $fullname поехал $direction, позиция: ${getPosition()}")
-    }
-
-    fun changeDirection(newDir: String) {
-        direction = newDir
-        println("Водитель $fullname сменил направление на $newDir")
-    }
-}
-
 fun main() {
     val humans = arrayOf(
         Human("Штейнбрехер Софья Владимировна", 20, 1.0, group_number = 433),
@@ -67,7 +17,8 @@ fun main() {
 
     val all = humans + driver
 
-    val threads = all.map { obj -> thread {
+    val threads = all.map { obj ->
+        thread {
             for (second in 1..time) {
                 Thread.sleep(300)
                 if (obj is Driver) {
